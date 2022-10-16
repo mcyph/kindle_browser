63bf52c64b165e9a07c99ac6fe838ba9a9f93645
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2022-10-16 15:42:55 +1100

    bugfix

--- src/process_image_for_output.py
+++ src/process_image_for_output.py
@@ -1,18 +1,19 @@
 import numpy
 import array
 from PIL import Image
-from numba import jit, uint, uint32
+from numba import jit, uint8, uint64
 from numba.typed import List
 
 
 @jit(nopython=True,
-     signature_or_function=uint[:](uint[:], uint[:]),
-     locals={'current_item': uint,
-             'current_count': uint,
-             'DIVISOR': uint,
-             'SINGLE_VALUES_FROM': uint,
-             'y': uint32})
-def run_length_encode(out_array, in_array):
+     signature_or_function=uint64(uint8[:], uint8[:], uint64),
+     locals={'current_item': uint8,
+             'current_count': uint8,
+             'DIVISOR': uint8,
+             'SINGLE_VALUES_FROM': uint8,
+             'y': uint64,
+             'x': uint64})
+def run_length_encode(out_array, in_array, num_items):
     DIVISOR = 64  # = 4 individual shades
     # May as well reserve values from a certain number for single items
     SINGLE_VALUES_FROM = (255 // DIVISOR) + 1
@@ -21,7 +22,7 @@ def run_length_encode(out_array, in_array):
     current_count = 0
     y = 0
 
-    for x in range(in_array.shape[0]):
+    for x in range(num_items):
         item = in_array[x] // DIVISOR  # Note double-division here!
 
         if item == current_item and current_count < 255:
@@ -54,16 +55,17 @@ def run_length_encode(out_array, in_array):
             out_array[y] = current_count
             y += 1
 
-    return out_array[:y-1]
+    return y
 
 
 def process_image_for_output(image: Image):
     print("Processing image for output...", end='')
-    print("IMAGE SIZE:", image.size)
-    data = numpy.asarray(image.convert('L', dither=Image.NONE)).flatten()
-    out_array = numpy.ndarray(shape=(data[0]*4), dtype=numpy.uint8)
-    out_array = run_length_encode(out_array, data)
-    data = out_array.asbytes()
+    #print("IMAGE SIZE:", image.size)
+    in_array = numpy.asarray(image.convert('L', dither=Image.NONE)).flatten()
+    out_array = numpy.ndarray(shape=(in_array.shape[0]*2,),
+                              dtype=numpy.uint8)
+    y = run_length_encode(out_array, in_array, in_array.shape[0])
+    data = out_array[:y].tobytes()
     print(' [Done]')
     return data
 
